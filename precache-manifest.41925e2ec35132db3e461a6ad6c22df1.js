ebe20bbe8128d29b8ff1f79f4a5fa79f7a863661
limchyo <limcheaoh@naver.com>
2019-01-13 14:31:55 +0900

    Updates

---
+++ precache-manifest.41925e2ec35132db3e461a6ad6c22df1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c8f689986438515d2777",
+    "url": "/movie_app/static/js/main.c8f68998.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "c8f689986438515d2777",
+    "url": "/movie_app/static/css/main.d714e5ef.chunk.css"
+  },
+  {
+    "revision": "34fc13a9422f108bc240db6fc19f5c7c",
+    "url": "/movie_app/index.html"
+  }
+];
